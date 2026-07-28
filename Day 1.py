3c74abc5c01a7dec65e3d15fcb0fde25181d38ba
madhansambari <madhan2990@gmail.com>
2026-07-28 15:18:04 +0530

    madhanday1

---
+++ Day 1.py
@@ -0,0 +1,26 @@
+print(' Hello Code Gnan')
+#IDLE is Color Coding Env
+# hastag is used as single Line comment
+# We triple quotes (single/double);;;,""" as multiline comments
+# print('hello gana')
+'''
+Tokens --> A smallest unit in a program. There are 5 types of tokens....
+
+-->Keywords
+-->identifiers
+-->
+literals
+-->operators
+--> punctuators
+
+'''
+
+name ='codegnan'
+print(name)
+email_id='madhan2990@gmail.com'
+print(email_id)
+phone_no ='918218508'
+print(phone_no)
+college_name ='cmr eng college'
+print(college_name)
+      
