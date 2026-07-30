3d67c69fca71610438d5957cf7d6a338dbfa0610
madhansambari <madhan2990@gmail.com>
2026-07-30 14:22:04 +0530

    Input formatting

---
+++ Day.3.py
@@ -0,0 +1,131 @@
+'''
+#numeric data type --> int,float,complex along with boolean
+
+#input formatting  --> accepting input from the user --> input()
+
+#accpting integer int fro user
+
+#by default input() accepts any input --> str
+
+#int (input()) -->will accept only integers
+
+age = int(input('enter the age:'))
+print(age)
+print(type(age))
+
+Age = float(input('enter the Age:-'))
+print(Age)
+print(type(Age))
+
+aGE = complex(input('enter the aGE::-'))
+print(aGE)
+print(type(aGE))
+
+name = input("enter ur name ")
+print(name)
+print(type(name))
+
+#accept group of values
+
+name = input("enter ur name ")
+class = input(""
+'''
+'''
+a= input().split()
+print(a)
+
+'''
+'''
+a=input().split(',')
+print(a)
+
+
+a=input().split()
+print(a)
+
+# now we want to accept 2 value from user
+age,salary= map(int
+'''  '''
+# Float of integers,
+marks = list(map(float,input("enter the values").split(',')))
+print(marks)
+'''
+'''
+#accepting input from user -->int , float  -->n input formatting
+
+#operators -->operators perform operations between values
+#(operands)
+#7 types -->arithmetic , assignment,comparision(relationship)
+#membership, identity ,logical, bitwise
+
+#arithemetic operators --> arithmetic operations
+#+,-,/,*
+print(5+3)
+print(5-3)
+print(5*3)
+print(5/3)
+print(5//3)
+print(5%3)
+print(5**3)
+
+
+#task --> accept integer input as length, breadth --> find the
+#area of rectangle 
+#area= length * breadth
+
+# Accept integer input as length and breadth
+length = int(input("Enter the length of rectangle: "))
+breadth = int(input("Enter the breadth of rectangle: "))
+
+# Calculate area
+area = length * breadth
+
+# Print result
+print("Area of rectangle:", area)
+'''
+#assinment operators--> assign the values
+# =,+=,-=
+
+a=45
+print(a)
+#update the value of a
+a=a+5 #a+=5
+print(a)
+b=35
+b += a #b=b=a
+print(b)
+
+'''
+>
+<
+==
+!=
+<=
+>=
+'''
+age=25
+print(age==25)
+print(age!=25)
+print(age<25)
+print(age>25)
+print(age<=25)
+print(age>=25)
+print(age==25)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day.3..py
@@ -0,0 +1,172 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+============= RESTART: C:/Users/madhan/Desktop/DA-HYD-023/Day.3.py =============
+enter the age:22
+22
+<class 'int'>
+enter the Age:-55
+55.0
+<class 'float'>
+enter the aGE::-66
+(66+0j)
+<class 'complex'>
+enter ur name madhan
+madhan
+<class 'str'>
+
+
+
+
+
+
+a= input().split()
+madhan codegnan data analysis
+a
+['madhan', 'codegnan', 'data', 'analysis']
+b= input().split(. . .)
+SyntaxError: invalid syntax
+SyntaxError: invalid syntax
+SyntaxError: invalid syntax
+b = input().split('/')
+madhan code gnan data ana
+d
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    d
+NameError: name 'd' is not defined. Did you mean: 'id'?
+b
+['madhan code gnan data ana']
+
+==== RESTART: C:/Users/madhan/Desktop/DA-HYD-023/Day.3.py ====
+code gnan 
+['code', 'gnan']
+madhan kuamr
+SyntaxError: invalid syntax
+SyntaxError: invalid syntax
+SyntaxError: invalid syntax
+frase
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    frase
+NameError: name 'frase' is not defined
+wf
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    wf
+NameError: name 'wf' is not defined
+ffe rwvw b re
+SyntaxError: invalid syntax
+
+==== RESTART: C:/Users/madhan/Desktop/DA-HYD-023/Day.3.py ====
+code gnan can be coded as codegnan
+['code gnan can be coded as codegnan']
+
+==== RESTART: C:/Users/madhan/Desktop/DA-HYD-023/Day.3.py ====
+code gnan can be coded as codegnan
+['code gnan can be coded as codegnan']
+
+[]
+
+==== RESTART: C:/Users/madhan/Desktop/DA-HYD-023/Day.3.py ====
+code gnan can be coded as codegnan
+['code gnan can be coded as codegnan']
+code gnan can be coded as codegnan
+['code', 'gnan', 'can', 'be', 'coded', 'as', 'codegnan']
+
+==== RESTART: C:/Users/madhan/Desktop/DA-HYD-023/Day.3.py ====
+enter the values 22 55 58  66 8 
+Traceback (most recent call last):
+  File "C:/Users/madhan/Desktop/DA-HYD-023/Day.3.py", line 50, in <module>
+    marks = list(map(float,input("enter the values").split('')))
+ValueError: empty separator
+
+==== RESTART: C:/Users/madhan/Desktop/DA-HYD-023/Day.3.py ====
+enter the values22,88,99,66,85,6,752,6
+[22.0, 88.0, 99.0, 66.0, 85.0, 6.0, 752.0, 6.0]
+
+==== RESTART: C:/Users/madhan/Desktop/DA-HYD-023/Day.3.py ====
+8
+2
+15
+1.6666666666666667
+1
+2
+
+==== RESTART: C:/Users/madhan/Desktop/DA-HYD-023/Day.3.py ====
+8
+2
+15
+1.6666666666666667
+1
+2
+125
+
+==== RESTART: C:/Users/madhan/Desktop/DA-HYD-023/Day.3.py ====
+8
+2
+15
+1.6666666666666667
+1
+2
+125
+Traceback (most recent call last):
+  File "C:/Users/madhan/Desktop/DA-HYD-023/Day.3.py", line 75, in <module>
+    area= l * b
+NameError: name 'l' is not defined
+
+==== RESTART: C:/Users/madhan/Desktop/DA-HYD-023/Day.3.py ====
+8
+2
+15
+1.6666666666666667
+1
+2
+125
+Traceback (most recent call last):
+  File "C:/Users/madhan/Desktop/DA-HYD-023/Day.3.py", line 75, in <module>
+    area= length * breadth
+NameError: name 'length' is not defined
+
+==== RESTART: C:/Users/madhan/Desktop/DA-HYD-023/Day.3.py ====
+8
+2
+15
+1.6666666666666667
+1
+2
+125
+Traceback (most recent call last):
+  File "C:/Users/madhan/Desktop/DA-HYD-023/Day.3.py", line 75, in <module>
+    area= length * breadth
+NameError: name 'length' is not defined
+>>> 
+==== RESTART: C:/Users/madhan/Desktop/DA-HYD-023/Day.3.py ====
+8
+2
+15
+1.6666666666666667
+1
+2
+125
+Enter the length of rectangle: 5
+Enter the breadth of rectangle: 6
+Area of rectangle: 30
+>>> 
+==== RESTART: C:/Users/madhan/Desktop/DA-HYD-023/Day.3.py ====
+45
+50
+85
+>>> 
+==== RESTART: C:/Users/madhan/Desktop/DA-HYD-023/Day.3.py ====
+45
+50
+
+... 85
+True
+False
+False
+False
+True
+True
+True
